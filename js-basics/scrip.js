e76bda3cf8af2c4a85df607532bb5297c173c12f
Nebil <nebilbromance@gmail.com>
2025-06-21 12:56:38 +0300

    Initial commit for My_First_Portfolio

---
+++ js-basics/scrip.js
@@ -0,0 +1,31 @@
+const name='Nebil';
+console.log(name)
+console.log(typeof(name))
+console.log('separators')
+const age=22;
+console.log(age)
+console.log(typeof(age))
+console.log('separators')
+const isStudent=true;
+console.log(isStudent)
+console.log(typeof(isStudent))
+console.log('separators')
+const address={
+    city:'Ethiopia',
+    country:'Agaro'
+
+}
+console.log(address)
+console.log(typeof(address))
+console.log('separators')
+const hobbies = ["Reading", "Coding", "Football", "Drawing", "Watching movies"];
+console.log(hobbies)
+console.log(typeof(hobbies))
+console.log('separators')
+let undefinedVariable;
+console.log(undefinedVariable)
+console.log(typeof(undefinedVariable))
+console.log('separators')
+let nullVariable=null;
+console.log(nullVariable)
+console.log(typeof(nullVariable))
